e1167046c79b2b5d63ac24900585b03e7acb7a03
Anuj Thakkelapally <anuj.thakkelapally@protonmail.com>
2026-05-12 21:30:19 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  // setup a buffer
+  char buffer[1024];
+  // write from file descriptor into buffer
+  fgets(buffer, 1024, stdin);
+  // clean the buffer for string functions
+  char *ptr = strchr(buffer, '\n');
+  *ptr = '\0';
+  // print the buffer
+  printf("%s: command not found\n", buffer);
+
 
   return 0;
 }
